d6eaae32bb59458f1c66f6fdfbfcec4f567595bd
Batyrkhan <b.tokmyrza@jusan.kz>
2023-08-19 18:39:39 +0600

    Setting Up the Module Structure

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Calory Tracker"
+include(":app")
+include(":core")
+include(":onboarding")
+include(":onboarding:onboarding_presentation")
+include(":onboarding:onboarding_domain")
+include(":onboarding:onboarding_data")
+include(":tracker")
+include(":tracker:tracker_presentation")
+include(":tracker:tracker_data")
+include(":tracker:tracker_domain")
