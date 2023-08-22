1ea47d81976dd7a36e5907cb0806beebbb747551
Batyrkhan <b.tokmyrza@jusan.kz>
2023-08-22 16:54:08 +0600

    DefaultPreferences code cleanup

--- core-data/src/main/java/kz/btokmyrza/core_data/preferences/DefaultPreferences.kt
+++ core-data/src/main/java/kz/btokmyrza/core_data/preferences/DefaultPreferences.kt
@@ -11,59 +11,41 @@ class DefaultPreferences(
     private val sharedPreferences: SharedPreferences,
 ) : Preferences {
 
-    override fun saveGender(gender: Gender) {
-        sharedPreferences.edit()
-            .putString(Preferences.KEY_GENDER, gender.name)
-            .apply()
-    }
+    override fun saveGender(gender: Gender) = sharedPreferences.edit()
+        .putString(Preferences.KEY_GENDER, gender.name)
+        .apply()
 
-    override fun saveAge(age: Int) {
-        sharedPreferences.edit()
-            .putInt(Preferences.KEY_AGE, age)
-            .apply()
-    }
+    override fun saveAge(age: Int) = sharedPreferences.edit()
+        .putInt(Preferences.KEY_AGE, age)
+        .apply()
 
-    override fun saveWeight(weight: Float) {
-        sharedPreferences.edit()
-            .putFloat(Preferences.KEY_WEIGHT, weight)
-            .apply()
-    }
+    override fun saveWeight(weight: Float) = sharedPreferences.edit()
+        .putFloat(Preferences.KEY_WEIGHT, weight)
+        .apply()
 
-    override fun saveHeight(height: Int) {
-        sharedPreferences.edit()
-            .putInt(Preferences.KEY_HEIGHT, height)
-            .apply()
-    }
+    override fun saveHeight(height: Int) = sharedPreferences.edit()
+        .putInt(Preferences.KEY_HEIGHT, height)
+        .apply()
 
-    override fun saveActivityLevel(activityLevel: ActivityLevel) {
-        sharedPreferences.edit()
-            .putString(Preferences.KEY_ACTIVITY_LEVEL, activityLevel.name)
-            .apply()
-    }
+    override fun saveActivityLevel(activityLevel: ActivityLevel) = sharedPreferences.edit()
+        .putString(Preferences.KEY_ACTIVITY_LEVEL, activityLevel.name)
+        .apply()
 
-    override fun saveGoalType(goalType: GoalType) {
-        sharedPreferences.edit()
-            .putString(Preferences.KEY_GOAL_TYPE, goalType.name)
-            .apply()
-    }
+    override fun saveGoalType(goalType: GoalType) = sharedPreferences.edit()
+        .putString(Preferences.KEY_GOAL_TYPE, goalType.name)
+        .apply()
 
-    override fun saveCarbRatio(carbRatio: Float) {
-        sharedPreferences.edit()
-            .putFloat(Preferences.KEY_CARB_RATIO, carbRatio)
-            .apply()
-    }
+    override fun saveCarbRatio(carbRatio: Float) = sharedPreferences.edit()
+        .putFloat(Preferences.KEY_CARB_RATIO, carbRatio)
+        .apply()
 
-    override fun saveProteinRatio(proteinRatio: Float) {
-        sharedPreferences.edit()
-            .putFloat(Preferences.KEY_PROTEIN_RATIO, proteinRatio)
-            .apply()
-    }
+    override fun saveProteinRatio(proteinRatio: Float) = sharedPreferences.edit()
+        .putFloat(Preferences.KEY_PROTEIN_RATIO, proteinRatio)
+        .apply()
 
-    override fun saveFatRatio(fatRatio: Float) {
-        sharedPreferences.edit()
-            .putFloat(Preferences.KEY_FAT_RATIO, fatRatio)
-            .apply()
-    }
+    override fun saveFatRatio(fatRatio: Float) = sharedPreferences.edit()
+        .putFloat(Preferences.KEY_FAT_RATIO, fatRatio)
+        .apply()
 
     override fun loadUserInfo(): UserInfo = UserInfo(
         gender = getGender(),
@@ -77,11 +59,10 @@ class DefaultPreferences(
         fatRatio = getFatRatio(),
     )
 
-    private fun getGender(): Gender =
-        sharedPreferences
-            .getString(Preferences.KEY_GENDER, null)
-            .orEmpty()
-            .let(Gender::fromString)
+    private fun getGender(): Gender = sharedPreferences
+        .getString(Preferences.KEY_GENDER, null)
+        .orEmpty()
+        .let(Gender::fromString)
 
     private fun getAge(): Int = sharedPreferences.getInt(Preferences.KEY_AGE, -1)
 
@@ -89,17 +70,15 @@ class DefaultPreferences(
 
     private fun getHeight(): Int = sharedPreferences.getInt(Preferences.KEY_HEIGHT, -1)
 
-    private fun getActivityLevel(): ActivityLevel =
-        sharedPreferences
-            .getString(Preferences.KEY_ACTIVITY_LEVEL, null)
-            .orEmpty()
-            .let(ActivityLevel::fromString)
+    private fun getActivityLevel(): ActivityLevel = sharedPreferences
+        .getString(Preferences.KEY_ACTIVITY_LEVEL, null)
+        .orEmpty()
+        .let(ActivityLevel::fromString)
 
-    private fun getGoalType(): GoalType =
-        sharedPreferences
-            .getString(Preferences.KEY_GOAL_TYPE, null)
-            .orEmpty()
-            .let(GoalType::fromString)
+    private fun getGoalType(): GoalType = sharedPreferences
+        .getString(Preferences.KEY_GOAL_TYPE, null)
+        .orEmpty()
+        .let(GoalType::fromString)
 
     private fun getCarbRatio(): Float = sharedPreferences.getFloat(Preferences.KEY_CARB_RATIO, -1f)
 
