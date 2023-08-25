6c264842c610b2c8e596d1cbceae211e240b32c9
Batyrkhan <b.tokmyrza@jusan.kz>
2023-08-25 15:26:31 +0600

    Tracked Food Item Widget

--- tracker/tracker_presentation/src/main/java/kz/btokmyrza/calorytracker/tracker_presentation/feature/overview/TrackerOverviewScreen.kt
+++ tracker/tracker_presentation/src/main/java/kz/btokmyrza/calorytracker/tracker_presentation/feature/overview/TrackerOverviewScreen.kt
@@ -1,22 +1,30 @@
 package kz.btokmyrza.calorytracker.tracker_presentation.feature.overview
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import kz.btokmyrza.calorytracker.core.R
 import kz.btokmyrza.calorytracker.core.util.UiEvent
 import kz.btokmyrza.calorytracker.core_ui.theme.CaloryTrackerTheme
 import kz.btokmyrza.calorytracker.core_ui.theme.LocalSpacing
 import kz.btokmyrza.calorytracker.tracker_presentation.event.TrackerOverviewEvent
+import kz.btokmyrza.calorytracker.tracker_presentation.feature.overview.components.AddButton
 import kz.btokmyrza.calorytracker.tracker_presentation.feature.overview.components.ExpandableMeal
+import kz.btokmyrza.calorytracker.tracker_presentation.feature.overview.components.TrackedFoodItem
 import kz.btokmyrza.calorytracker.tracker_presentation.feature.overview.components.header.NutrientsHeader
 import kz.btokmyrza.calorytracker.tracker_presentation.model.MealDvo
+import kz.btokmyrza.calorytracker.tracker_presentation.model.TrackedFoodDvo
 import kz.btokmyrza.calorytracker.tracker_presentation.model.defaultMeals
 import java.time.LocalDate
 
@@ -29,6 +37,10 @@ fun TrackerOverviewScreen(
     TrackerOverviewScreenContent(
         uiState = uiState,
         onToggleMealClick = { viewModel.onEvent(TrackerOverviewEvent.OnToggleMealClick(it)) },
+        onDeleteTrackedFoodClick = {
+            viewModel.onEvent(TrackerOverviewEvent.OnDeleteTrackedFoodClick(it))
+        },
+        onAddFoodClick = { viewModel.onEvent(TrackerOverviewEvent.OnAddFoodClick(it)) },
     )
 }
 
@@ -36,9 +48,10 @@ fun TrackerOverviewScreen(
 private fun TrackerOverviewScreenContent(
     uiState: TrackerOverviewUiState,
     onToggleMealClick: (MealDvo) -> Unit,
+    onDeleteTrackedFoodClick: (TrackedFoodDvo) -> Unit,
+    onAddFoodClick: (MealDvo) -> Unit,
 ) {
     val spacing = LocalSpacing.current
-    val context = LocalContext.current
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
@@ -53,10 +66,47 @@ private fun TrackerOverviewScreenContent(
                 meal = meal,
                 onToggleClick = { onToggleMealClick(meal) },
                 content = {
-
+                    ExpandableMealContent(
+                        trackedFoods = uiState.trackedFoods,
+                        meal = meal,
+                        onDeleteTrackedFoodClick = onDeleteTrackedFoodClick,
+                        onAddFoodClick = onAddFoodClick,
+                    )
                 },
             )
         }
+    }
+}
+
+@Composable
+private fun ExpandableMealContent(
+    trackedFoods: List<TrackedFoodDvo>,
+    meal: MealDvo,
+    onDeleteTrackedFoodClick: (TrackedFoodDvo) -> Unit,
+    onAddFoodClick: (MealDvo) -> Unit,
+) {
+    val spacing = LocalSpacing.current
+    val context = LocalContext.current
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = spacing.spaceSmall),
+    ) {
+        trackedFoods.forEach { food ->
+            TrackedFoodItem(
+                trackedFood = food,
+                onDeleteClick = { onDeleteTrackedFoodClick(food)  },
+            )
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+        }
+        AddButton(
+            modifier = Modifier.fillMaxWidth(),
+            text = stringResource(
+                id = R.string.add_meal,
+                formatArgs = arrayOf(meal.name.asString(context)),
+            ),
+            onClick = { onAddFoodClick(meal) },
+        )
     }
 }
 
@@ -79,6 +129,8 @@ private fun TrackerOverviewScreenPreview() {
                 meals = defaultMeals,
             ),
             onToggleMealClick = {},
+            onDeleteTrackedFoodClick = {},
+            onAddFoodClick = {},
         )
     }
 }
