bebb7acf6cc58db8694afa05443750346b0f9b96
Batyrkhan <b.tokmyrza@jusan.kz>
2023-08-26 12:26:36 +0600

    DefaultTrackerRepository Test

--- tracker/tracker_data/src/test/java/kz/btokmyrza/tracker_data/repository/DefaultTrackerRepositoryTest.kt
+++ tracker/tracker_data/src/test/java/kz/btokmyrza/tracker_data/repository/DefaultTrackerRepositoryTest.kt
@@ -19,7 +19,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
 
-internal class DefaultTrackerRepositoryUnitTest {
+internal class DefaultTrackerRepositoryTest {
 
     private lateinit var repository: DefaultTrackerRepository
     private lateinit var mockWebServer: MockWebServer
